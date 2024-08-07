df5a48a22a7cedfb68075907bc0a84f35bf4c02a
Saleem3097 <md.saleem3097@gmail.com>
2024-08-08 00:11:56 +0530

    fetch-Api sideEffects practice

--- src/App.js
+++ src/App.js
@@ -1,94 +1,35 @@
-import { useState } from 'react';
+import {  useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  const [newTask, setTask] = useState("");
-  const [toDoList, setToDoList] = useState([]);
+   const [excuse, setExcuse]= useState([]);
 
-  const getData = (event) => {
-    setTask(event.target.value);
-  };
+  useEffect(()=>{fetchApi()}, [])
 
-  const addTaskList = () => {
-    const gotList = {
-      id: toDoList.length === 0 ? 1 : toDoList[toDoList.length - 1].id + 1,
-      content: newTask,
-      completed: false,
-      modification: false
-    };
-
-    setToDoList([...toDoList, gotList]);
-    setTask("");
-  };
-
-  const deleteTask = (id) => {
-    setToDoList(toDoList.filter((task) => task.id !== id));
-  };
-
-  const updateTask = (id) => {
-    setToDoList(toDoList.map((task) => {
-      if (task.id === id) {
-        return { ...task, modification: true };
-      } else {
-        return task;
-      }
-    }));
-  };
-
-  const handleChange = (event, id) => {
-    const updatedContent = event.target.value;
-    setToDoList(toDoList.map((task) => {
-      if (task.id === id) {
-        return { ...task, content: updatedContent };
-      } else {
-        return task;
-      }
-    }));
-  };
-
-  const handleKeyDown = (event, id) => {
-    if (event.key === 'Enter') {
-      setToDoList(toDoList.map((task) => {
-        if (task.id === id) {
-          return { ...task, modification: false };
-        } else {
-          return task;
+   const fetchApi = (category)=>{
+      axios.get(`https://excuser-three.vercel.app/v1/excuse/${category}`).then(
+        (res)=>{
+          console.log(res.data);
+          setExcuse(res.data);
         }
-      }));
-    }
-  };
-
-  return (
-    <>
-    <h1 className='App-heading'>TODOLIST WEB-APP</h1>
-    <div className="App">
-      <div className='form'>
-        <input type='text' value={newTask} onChange={getData} />
-        <button onClick={addTaskList}>Add Task</button>
+      )
+   }
+   return (
+   <div className='App'>
+      <div className='button'>
+        <button onClick={()=>fetchApi("party")}>Party</button>
+        <button onClick={()=>fetchApi("family")}>Family</button>
+        <button onClick={()=>fetchApi("office")}>Office</button>
       </div>
-      <div className='list'>
-        {toDoList.map((task) => (
-          <div key={task.id} className='task-item'>
-            {task.modification ? (
-              <input 
-                type="text" 
-                value={task.content}
-                onChange={(event) => handleChange(event, task.id)}
-                onKeyDown={(event) => handleKeyDown(event, task.id)}
-                className='task-input'
-              />
-            ) : (
-              <>
-                <h1 className='task-content'>{task.content}</h1>
-                <button className="delete" onClick={() => deleteTask(task.id)}>X</button>
-                <button className="update" onClick={() => updateTask(task.id)}>Update</button>
-              </>
-            )}
-          </div>
-        ))}
+      <div className='listData'>
+          {excuse.map(((data, index)=>(
+              <h1 key={index}>
+                  {data.excuse}
+              </h1>
+          )))}
       </div>
     </div>
-    </>
   );
 }
 
